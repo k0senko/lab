264a777cf4731d2abe3bc77e7cfb600e6ae13d80
k0senko <k0senkoi@outlook.com>
2024-07-01 07:44:02 +0300

    hello world

---
+++ 13laba_13var/13laba_13var.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <conio.h>
+using namespace std;
+
+// сортировка методом Шелла
+void ShellSort(int n, vector<int>& mass)
+{
+    int i, j, step;
+    int tmp;
+    for (step = n / 2; step > 0; step /= 2)
+        for (i = step; i < n; i++)
+        {
+            tmp = mass[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (tmp < mass[j - step])
+                    mass[j] = mass[j - step];
+                else
+                    break;
+            }
+            mass[j] = tmp;
+        }
+}
+
+int main()
+{
+    // ввод N
+    int N;
+    cout << "Input N: ";
+    cin >> N;
+
+    // выделение памяти под массив
+    vector<int> mass(N);
+
+    // ввод элементов массива
+    cout << "Input the array elements:\n";
+    for (int i = 0; i < N; i++)
+        cin >> mass[i];
+
+    // сортировка методом Шелла
+    ShellSort(N, mass);
+
+    // вывод отсортированного массива на экран
+    cout << "Sorted array:\n";
+    for (int i = 0; i < N; i++)
+        cout << mass[i] << " ";
+    cout << endl;
+
+    _getch();
+    return 0;
+}
